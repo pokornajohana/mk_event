7c6db988c53a53f2d752bb73091cdbbfbee93d39
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2025-02-24 18:11:18 +0100

    form mail changed, pictures added

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // Vybereme všechny odkazy, které odkazují na sekce na stránce
-  const links = document.querySelectorAll('nav a[href^="#"]');
+// Vybereme všechny odkazy, které odkazují na sekce na stránce
+const links = document.querySelectorAll('nav a[href^="#"]');
 
-  links.forEach((link) => {
-    link.addEventListener('click', function (e) {
-      e.preventDefault(); // Zabráníme výchozímu chování odkazu
+links.forEach((link) => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); // Zabráníme výchozímu chování odkazu
 
-      const targetId = link.getAttribute('href'); // Získáme ID cílové sekce
-      const targetElement = document.querySelector(targetId); // Vybereme cílový prvek
+    const targetId = link.getAttribute('href'); // Získáme ID cílové sekce
+    const targetElement = document.querySelector(targetId); // Vybereme cílový prvek
 
-      if (targetElement) {
-        // Plynulé scrollování
-        targetElement.scrollIntoView({
-          behavior: 'smooth', // Plynulé scrollování
-          block: 'start', // Zarovnat k začátku sekce
-        });
-      }
-    });
+    if (targetElement) {
+      // Plynulé scrollování
+      targetElement.scrollIntoView({
+        behavior: 'smooth', // Plynulé scrollování
+        block: 'start', // Zarovnat k začátku sekce
+      });
+    }
   });
 });
