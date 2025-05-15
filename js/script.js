0a5f254835fab757ecdadefbbc5ea0e64e182aba
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2025-05-15 22:39:27 +0200

    new pictures for agenda_card added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 // Vybereme všechny odkazy, které odkazují na sekce na stránce
-window.addEventListener("click", e => {
+window.addEventListener('click', (e) => {
   let link = e.target.closest('a');
-  if (link && link.getAttribute('href').startsWith("#")) {
+  if (link && link.getAttribute('href').startsWith('#')) {
     e.preventDefault();
 
     let targetElement = document.querySelector(link.getAttribute('href'));
     if (targetElement) {
       targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
+        behavior: 'smooth',
+        block: 'start',
       });
     }
   }
+});
+
+const agendaWrapper = document.querySelector('.agenda_wrapper');
+const agendaCards = document.querySelectorAll('.agenda_card');
+
+let scrollSpeed = 2; // Rychlost pohybu
+let isPaused = false;
+let animationFrame;
+
+// Přidání event listenerů pro zastavení a obnovení posunu
+agendaCards.forEach((card) => {
+  card.addEventListener('mouseenter', () => {
+    console.log('kurzor na prvku');
+    isPaused = true;
+    cancelAnimationFrame(animationFrame); // Zastaví animaci
+  });
+
+  card.addEventListener('mouseleave', () => {
+    console.log('kurzor opustil prvek');
+    isPaused = false;
+    autoScroll(); // Znovu spustí animaci
+  });
 });
